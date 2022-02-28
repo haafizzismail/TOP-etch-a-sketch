09190445e0d1a11012777fa4078e874195838e45
Haafizz Ismail <mdnurhaafizz@gmail.com>
2022-02-28 20:53:25 +0800

    add prompt user upon reset

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
 const gridContainer = document.querySelector('.grid-container');
 
-function createGrid(row, column) {
+createGrid(16);
+
+const reset = document.querySelector('.reset');
+reset.addEventListener('click', resetGrid);
+
+function createGrid(number) {
     let i = 0;
-    while (i < row) {
+    while (i < number) {
         const gridRow = document.createElement('div');
         gridRow.classList.add('grid-row');
         gridContainer.appendChild(gridRow);
         i++;
 
         let j = 0;
-        while (j < column) {
+        while (j < number) {
             const grid = document.createElement('div');
             grid.classList.add('grid-box');
             gridRow.appendChild(grid);
             j++;
         }
     }
+    const grids = document.querySelectorAll('.grid-box');
+
+    grids.forEach(grid => {
+        grid.addEventListener('mouseover', () => grid.style.backgroundColor = 'blue');
+    });
 }
 
 function resetGrid() {
+    const grids = document.querySelectorAll('.grid-box');
     grids.forEach(grid => grid.style.backgroundColor = 'white');
+    promptUser();
 }
 
-createGrid(16, 16);
-const grids = document.querySelectorAll('.grid-box');
-grids.forEach(grid => {
-    grid.addEventListener('mouseover', () => grid.style.backgroundColor = 'blue');
-});
+function promptUser() {
+    let a = 0;
+    do {
+        a = prompt("How many boxes for each side?");
+    } while (a > 100 || a < 1);
 
-const reset = document.querySelector('.reset');
-reset.addEventListener('click', resetGrid);
+    const grids = document.querySelectorAll('.grid-box');
+    const gridRows = document.querySelectorAll('.grid-row');
+    grids.forEach(grid => {
+        grid.remove();
+    })
+    gridRows.forEach(gridRow => {
+        gridRow.remove();
+    })
+    createGrid(a);
+    // removeGrid();
+}
